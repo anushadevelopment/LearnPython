6deee5cc853e8f9b35bc4b79c9501aa92233566b
anushadevelopment <anushadevelopment@gmail.com>
2021-02-15 10:15:47 +0800

    pythonfunctions

---
+++ function.py
@@ -0,0 +1,24 @@
+#def func():
+    #print ("first function")
+#   def func1():
+        #print ("seond function")
+#   def func2():
+        #print ("third function")
+#   func1()
+#   func2()
+#unc()
+#
+def func(n):
+    def fun1():
+        print("Sri")
+    def fun2():
+        print("Akshu")
+    if n ==1 :
+        return fun1()
+    else:
+        return fun2()
+a = func(1)
+b = func(2)
+#print (a)
+#print (b)
+
